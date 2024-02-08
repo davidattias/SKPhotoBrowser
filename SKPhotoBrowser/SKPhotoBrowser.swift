1fbbfd138030e1040fdb5a8b1877424ef111716f
David Attias <david.attias@me.com>
2024-02-08 20:14:42 +0100

    Tweak sharing.

--- SKPhotoBrowser/SKPhotoBrowser.swift
+++ SKPhotoBrowser/SKPhotoBrowser.swift
@@ -236,11 +236,11 @@ open class SKPhotoBrowser: UIViewController {
         }
         
         var activityItems: [AnyObject] = []
+	activityItems.append(underlyingImage)
 	if let shareExtraCaption = SKPhotoBrowserOptions.shareExtraCaption {
 		let caption = shareExtraCaption
 		activityItems.append(caption as AnyObject)
 	}
-	activityItems.append(underlyingImage)
         
         if let activityItemProvider = activityItemProvider {
             activityItems.append(activityItemProvider.item as AnyObject)
